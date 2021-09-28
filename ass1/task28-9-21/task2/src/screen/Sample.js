26e7b1da4d8b02ddc8d15556c6069665ce11bdaf
silpapsweft <silpa.ps@wefttechnologies.com>
2021-09-28 21:51:55 +0530

    added themecolor

--- ass1/task28-9-21/task2/src/screen/Sample.js
+++ ass1/task28-9-21/task2/src/screen/Sample.js
@@ -1,8 +1,71 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Image, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Entypo';
+const numColumns = 2;
+const Data = [
+    {
+        id: "1",
+        title: "FATURAMLNTOS",
+        imges: require('../images/i1.png')
+    },
+    {
+        id: "2",
+        title: "CADKA",
+        imges: require('../images/i2.png')
+    },
+    {
+        id: "3",
+        title: "EXTRATOS",
+        imges: require('../images/i3.png')
+    },
+    {
+        id: "4",
+        title: "TRANGHCH OFFLINE",
+        imges: require('../images/i4.png')
+    },
+    {
+        id: "5",
+        title: "VALORES RECEIVER",
+        imges: require('../images/i5.png')
+    },
+
+    {
+        id: "6",
+        title: "PAGER CONTAS",
+        imges: require('../images/i6.png')
+    },
+
+
+
+
+];
+const Item = ({ item, onPress, backgroundColor, tint, textColor }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.cards, backgroundColor]}>
+        <Image source={item.imges} style={[styles.cardImg, tint]} ></Image>
+        <Text style={[styles.cardText, textColor]}>{item.title}</Text>
+    </TouchableOpacity>
+);
+
+
 
 const Sample = () => {
+    const [selectedId, setSelectedId] = useState(null);
+
+    const renderItem = ({ item }) => {
+        const tintColor = item.id === selectedId ? "#fff" : "#fd7907";
+        const backgroundColor = item.id === selectedId ? "#fd7907" : "#fff";
+        const color = item.id === selectedId ? 'white' : '#8f8f8f';
+        return (
+            <Item
+                item={item}
+                onPress={() => setSelectedId(item.id)}
+                backgroundColor={{ backgroundColor }}
+                tint={{ tintColor }}
+                textColor={{ color }}
+            />
+        );
+    };
+
     return (
         <View style={{ flex: 1, }}>
             <View style={styles.header}>
@@ -24,39 +87,18 @@ const Sample = () => {
                     <Text style={styles.rs}>R$ <Text style={{ fontSize: 42, color: '#000' }}>302,36</Text></Text>
                 </View>
                 <View style={{ flex: 10, margin: 20 }}>
-                    <View style={{ flex: 2, flexDirection: 'row' }}>
-                        <View style={styles.cards}>
-                            <Image source={require('../images/i1.png')} style={styles.cardImg}></Image>
-                            <Text style={{ color: '#8f8f8f', paddingTop: 10 }}>FATURAMLNTOS</Text>
-                        </View>
-                        <View style={styles.cards}>
-                            <Image source={require('../images/i2.png')} style={styles.cardImg}></Image>
-                            <Text style={{ color: '#8f8f8f', paddingTop: 10 }}>CADKA</Text>
-                        </View>
-                    </View>
-                    <View style={{ flex: 2, flexDirection: 'row' }}>
-                        <View style={styles.cards}>
-                            <Image source={require('../images/i3.png')} style={styles.cardImg}></Image>
-                            <Text style={{ color: '#8f8f8f', paddingTop: 10 }}>EXTRATOS</Text>
-                        </View>
-                        <View style={styles.cards}>
-                            <Image source={require('../images/i4.png')} style={styles.cardImg}></Image>
-                            <Text style={{ color: '#8f8f8f', paddingTop: 10 }}>TRANGHCH OFFLINE</Text>
-                        </View>
-                    </View>
-                    <View style={{ flex: 2, flexDirection: 'row' }}>
-                        <View style={styles.cards}>
-                            <Image source={require('../images/i5.png')} style={styles.cardImg}></Image>
-                            <Text style={{ color: '#8f8f8f', paddingTop: 10 }}>VALORES RECEIVER</Text>
-                        </View>
-                        <View style={styles.cards}>
-                            <Image source={require('../images/i6.png')} style={styles.cardImg}></Image>
-                            <Text style={{ color: '#8f8f8f', paddingTop: 10 }}>PAGER CONTAS</Text>
-                        </View>
-                    </View>
+
+
+                    <FlatList
+                        data={Data}
+                        renderItem={renderItem}
+                        keyExtractor={(item) => item.id}
+                        extraData={selectedId}
+                        numColumns={numColumns}
+                    />
+
                 </View>
             </View>
-
         </View>
     )
 }
@@ -70,40 +112,52 @@ const styles = StyleSheet.create(
             paddingVertical: 20
         },
         headerText: {
-            color: '#fff', 
-            fontWeight: 'bold', 
+            color: '#fff',
+            fontWeight: 'bold',
             fontSize: 20
         },
-        nextSection:{ 
+        nextSection: {
             flex: 3,
-            backgroundColor: '#fff', 
-            margin: 20, 
-            borderRadius: 10, 
-            padding: 20, 
-            elevation: 8 
+            backgroundColor: '#fff',
+            margin: 20,
+            borderRadius: 10,
+            padding: 20,
+            elevation: 8
         },
-        saldoText:{ 
-            color: '#fd7907', 
-            fontWeight: 'bold', 
-            fontSize: 20 
+        saldoText: {
+            color: '#fd7907',
+            fontWeight: 'bold',
+            fontSize: 20
         },
-        rs:{ 
-            fontSize: 18, 
-            marginVertical: 10, 
-            color: '#8f8f8f' 
+        rs: {
+            fontSize: 18,
+            marginVertical: 10,
+            color: '#8f8f8f'
         },
-        cards:{ 
-            flex: 1, 
-            margin: 5, 
-            borderRadius: 10, 
-            backgroundColor: '#fff', 
-            elevation: 2, 
-            padding: 15 
+        cards: {
+            flex: 1,
+            margin: 5,
+            borderRadius: 10,
+            backgroundColor: '#fff',
+            elevation: 2,
+            padding: 15
         },
-        cardImg:{ 
-            width: 40, 
-            height: 42, 
-            tintColor: '#fd7907' 
+        cards1: {
+            flex: 1,
+            margin: 5,
+            borderRadius: 10,
+            backgroundColor: '#ccc',
+            elevation: 2,
+            padding: 15
+        },
+        cardImg: {
+            width: 40,
+            height: 42,
+            tintColor: '#fd7907'
+        },
+        cardText: {
+            color: '#8f8f8f',
+            paddingTop: 10
         },
     }
 )
